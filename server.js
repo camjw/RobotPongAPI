13d9e072027213b55212aaa69e8a24150b86f90c
Cameron Whitehead <camjw119@gmail.com>
2018-11-20 14:38:02 +0000

    Added folder structure to API

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 express = require('express');
 
-const model = require('./model/model.json')
 const cors = require('cors')
 const app = express();
 
 app.use(cors())
 
-app.get('/model', (req, res) => {
+let current_version = null
+
+app.get('/model/version:version_no', (req, res) => {
+  const model = require('./model/version' + req.params.version_no + '/javascript/model.json')
+  current_version = req.params.version_no
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(model));
 });
 
-app.get('/group:a-shard:b', (req, res) => {
-  res.sendfile('./model/group'+ req.params.a + '-shard' + req.params.b)
+app.get('/model/group:a-shard:b', (req, res) => {
+  res.sendfile('./model/version' + current_version +  '/javascript/group'+ req.params.a + '-shard' + req.params.b)
 })
 
 const PORT = process.env.PORT || 5000;
